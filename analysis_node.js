5c840e1e38f3e68fd83775e49ff569b80f309093
Nanfe05 <hernan05felipe@gmail.com>
2020-04-22 20:13:10 -0500

    settings

--- analysis_node.js
+++ analysis_node.js
@@ -3,7 +3,7 @@ const csv = require('@fast-csv/parse');
 
 let filepath, codeColumn, companyNameColumn, debitoColumn, creditoColumn, based;
 const values={};
-let currentCode;
+let currentCode = null;
 
 
 // Funcion Analisis
@@ -13,38 +13,48 @@ function Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColu
     .on('error', error => console.error(error))
     .on('data', row => {
         //Discover based 8 digits
+        let jump = false;
         if(row[codeColumn] !== ''){
             let name = row[codeColumn].split(' ');
-            if(name[0].length===based){
+            if(name[0].length===based ){
                 currentCode=name[0];
                 values[name[0]]={
                     codigo:name[0],
                     nombre:row[codeColumn].split(' ').splice(1,row[codeColumn].length).join(''),
-                    valor:0
+                    valor:0,
+                    debito:0,
+                    credito:0
                 };
+                jump = true;
             }
         }
         //Adding values if Name
         //DEBITO
-        if(row[companyNameColumn] !== ''){
-            
-
-            let name = row[codeColumn].split(' ');
-            //console.log(name);
-           if( name[0].length>based && name[0].length < based+4|| row[codeColumn] === ''){
-                //DEBITO
-                let debito = row[debitoColumn].replace(/,/g,'').replace(/-/g,'').replace(/\$/g,'');
-                if(!isNaN(parseInt(debito))){
-                    //Add Value
-                    values[currentCode]['valor']+= parseInt(debito);
-                    //console.log(parseInt(number));
-                }
-                //CREDITO
-                let credito = row[creditoColumn].replace(/,/g,'').replace(/-/g,'').replace(/\$/g,'');
-                if(!isNaN(parseInt(credito))){
-                    //Substract Value
-                    values[currentCode]['valor']-= parseInt(credito);
-                    //console.log(parseInt(number));
+        if(currentCode){
+            if(!jump){
+                if(row[companyNameColumn] !== ''){
+                    
+        
+                    let name = row[codeColumn].split(' ');
+                    //console.log(name);
+                   if( name[0].length>based && name[0].length < based+4|| row[codeColumn] === ''){
+                        //DEBITO
+                        let debito = row[debitoColumn].replace(/,/g,'').replace(/-/g,'').replace(/\$/g,'');
+                        if(!isNaN(parseInt(debito))){
+                            //Add Value
+                            values[currentCode]['valor']+= parseInt(debito);
+                            values[currentCode]['debito'] += parseInt(debito);
+                            //console.log(parseInt(number));
+                        }
+                        //CREDITO
+                        let credito = row[creditoColumn].replace(/,/g,'').replace(/-/g,'').replace(/\$/g,'');
+                        if(!isNaN(parseInt(credito))){
+                            //Substract Value
+                            values[currentCode]['valor']-= parseInt(credito);
+                            values[currentCode]['credito'] += parseInt(credito);
+                            //console.log(parseInt(number));
+                        }
+                    }
                 }
             }
         }
@@ -58,8 +68,8 @@ function Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColu
     });
 }
 
-// 61 Gasto 2017
-// filepath = './61gasto2017.csv';
+// 61 Gasto 2018
+// filepath = './61gasto2018.csv';
 // codeColumn = 0;
 // companyNameColumn=3;
 // debitoColumn =7;
@@ -67,29 +77,38 @@ function Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColu
 // based = 8;
 // Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColumn,based,true);
 
-// // COSTO 2017
-// filepath = './costo2017.csv';
-// codeColumn = 0;
-// companyNameColumn=5;
-// debitoColumn =6;
-// creditoColumn =7;
-// based = 8;
-// Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColumn,based,true);
-
-// 51 Gasto 2017
-filepath = './51gasto2017.csv';
-codeColumn = 0;
-companyNameColumn=3;
-debitoColumn =7;
-creditoColumn =8;
-based = 4;
-Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColumn,based,true);
-
-// // // GASTO 2017
-// filepath = './gasto2017.csv';
+// COSTO 2018
+// filepath = './costo2018.csv';
 // codeColumn = 0;
 // companyNameColumn=5;
 // debitoColumn =6;
 // creditoColumn =7;
 // based = 4;
 // Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColumn,based,true);
+
+// // MATERIA PRIMA 2018
+// filepath = './2018materiaprima.csv';
+// codeColumn = 0;
+// companyNameColumn=5;
+// debitoColumn =6;
+// creditoColumn =7;
+// based = 4;
+// Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColumn,based,true);
+
+// 51 Gasto 2018
+// filepath = './51gasto2018.csv';
+// codeColumn = 0;
+// companyNameColumn=3;
+// debitoColumn =6;
+// creditoColumn =7;
+// based = 4;
+// Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColumn,based,true);
+
+// GASTO 2018
+filepath = './gasto2018.csv';
+codeColumn = 0;
+companyNameColumn=5;
+debitoColumn =6;
+creditoColumn =7;
+based = 4;
+Analisis(filepath,codeColumn,companyNameColumn,debitoColumn,creditoColumn,based,true);
